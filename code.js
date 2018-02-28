a8ed5674c17269961308a8bf68ee1f50701277b0
wouterdhooghe <dhooghew@gmail.com>
2018-03-01 00:49:31 +0100

    spatie voor ontbinding in priemfactoren

--- code.js
+++ code.js
@@ -264,6 +264,38 @@ function flattenOp(eq) {
     return neweq;
 };
 
+// least factor functie van internet. Werkt voor integers tot 2^53
+// find the least factor in n by trial division
+function leastFactor(n) {
+    if (isNaN(n) || !isFinite(n)) return NaN;  
+    if (n==0) return 0;  
+    if (n%1 || n*n<2) return 1;
+    if (n%2==0) return 2;  
+    if (n%3==0) return 3;  
+    if (n%5==0) return 5;  
+    var m = Math.sqrt(n);
+    for (var i=7;i<=m;i+=30) {
+     if (n%i==0)      return i;
+     if (n%(i+4)==0)  return i+4;
+     if (n%(i+6)==0)  return i+6;
+     if (n%(i+10)==0) return i+10;
+     if (n%(i+12)==0) return i+12;
+     if (n%(i+16)==0) return i+16;
+     if (n%(i+22)==0) return i+22;
+     if (n%(i+24)==0) return i+24;
+    }
+    return n;
+   }
+
+// gebruikt leastFactor om een string terug te geven met alle priemfactoren
+function factor(n) {
+    if (isNaN(n) || !isFinite(n) || n%1!=0 || n==0) return ''+n;
+    if (n<0) return '-'+factor(-n);
+    var minFactor = leastFactor(n);
+    if (n==minFactor) return ''+n;
+    return minFactor+'*'+factor(n/minFactor);
+   }
+
 //************************************* */
 // SELECTIES
 //************************************* */
@@ -728,6 +760,24 @@ function replaceWithEquality() {
     updateLatex(equation);
 }
 
+function spaceBar(eq) {
+    selectAdress = adresses('Select', eq)[0];
+    selectNode = readAtAdress(selectAdress, eq);
+    if (Number.isInteger(parseFloat(selectNode.args[0].value))) {
+        number = selectNode.args[0].value;
+        priemOntbinding = math.parse(factor(number));
+        equation = substituteSelected(selectIt(priemOntbinding),eq);
+        updateLatex(equation);
+
+    } else if (Number.isInteger(math.eval(selectNode.args[0].toString()))) {
+        uitkomstString = math.eval(selectNode.args[0].toString());
+        console.log(uitkomstString);
+        equation = substituteSelected('Select('+uitkomstString+')',eq);
+        updateLatex(equation);
+     };
+
+}
+
 function leftSelect(eq) {
 
     // bereken het nieuwe adres voor select
@@ -920,7 +970,7 @@ function inputDigit (digit,eq) {
             updateLatex(eq);
             };
         } else {
-            substituteSelected(selectIt(''+ digit), equation);
+            substituteSelected(selectIt(''+ digit), eq);
     };    
 };
 
