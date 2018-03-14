e47a31355c20d691d6e0774dd1a804b9ee0604f2
wouterdhooghe <dhooghew@gmail.com>
2018-03-14 09:47:55 +0100

    opgaven.js toegevoegd

---
+++ opgaven.js
@@ -0,0 +1,28 @@
+opgaven = [
+    {
+        naam: "typische 1e graad",
+        opgave: "Select(Plus(Times(2, x), unaryMinus(1))) == y",
+
+    },
+    {
+        naam: "typische 2e graad",
+        opgave: "Plus(pow(x, 2), Times(10, x), Select(25)) == y"
+    },
+    {
+        naam: "coole 3e graad vgl",
+        opgave: "Times(pow(x, 2), Plus(x, Select(34))) == pow(y, 2)"
+    }
+    {
+        naam: "typische rationale functie",
+        opgave: "Select(Times(2, x) / Plus(1, pow(x, 2))) == y"
+    },
+    {
+        naam: "typische trigonometrische functie",
+        opgave: "Times(sin(x), cos(Select(Times(2, x)))) == y"
+    },
+    {
+        naam: "constante functie",
+        opgave: "pow(26 / Select(52), 4) == pow(y, 2)"
+    }
+
+]
