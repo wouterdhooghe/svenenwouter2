9e545bee419d6d7d8ee648a6b043ba6005fc33bf
wouterdhooghe <dhooghew@gmail.com>
2018-06-17 09:15:46 +0200

    buttonclick onclick werkt

--- code.js
+++ code.js
@@ -166,31 +166,31 @@ customFunctions.Select.toTex = function(node, options) {
 // f knoppen
 //************************************* */
 
-function f1_hold(eq) {}
-function f2_hold(eq) {}
-function f3_hold(eq) {}
-function f4_hold(eq) {}
-function f5_hold(eq) {}
-function f6_hold(eq) {}
-function f7_hold(eq) {}
-function f8_hold(eq) {}
-function f9_hold(eq) {}
-function f10_hold(eq) {}
-function f11_hold(eq) {}
-function f12_hold(eq) {}
+function f1_hold(eq) {};
+function f2_hold(eq) {};
+function f3_hold(eq) {};
+function f4_hold(eq) {};
+function f5_hold(eq) {};
+function f6_hold(eq) {};
+function f7_hold(eq) {};
+function f8_hold(eq) {};
+function f9_hold(eq) {};
+function f10_hold(eq) {};
+function f11_hold(eq) {};
+function f12_hold(eq) {};
 
-function f1_release(eq) { f1(equation)}
-function f2_release(eq) { f2(equation)}
-function f3_release(eq) { f3(equation)}
-function f4_release(eq) { f4(equation)}
-function f5_release(eq) { f5(equation)}
-function f6_release(eq) { f6(equation)}
-function f7_release(eq) { f7(equation)}
-function f8_release(eq) { f8(equation)}
-function f9_release(eq) { f9(equation)}
-function f10_release(eq) {f10(equation)}
-function f11_release(eq) {f11(equation)}
-function f12_release(eq) {f12(equation)}
+function f1_release(eq) { f1(eq)};
+function f2_release(eq) { f2(eq)};
+function f3_release(eq) { f3(eq)};
+function f4_release(eq) { f4(eq)};
+function f5_release(eq) { f5(eq)};
+function f6_release(eq) { f6(eq)};
+function f7_release(eq) { f7(eq)};
+function f8_release(eq) { f8(eq)};
+function f9_release(eq) { f9(eq)};
+function f10_release(eq) {f10(eq)};
+function f11_release(eq) {f11(eq)};
+function f12_release(eq) {f12(eq)};
 
 
 function f1(eq) {
@@ -249,7 +249,9 @@ function naarplus(eq) {
  /// idee: voor alle regels in naarPlus, bereken de uitkomst. Alle uitkomsten presenteren aan de gebruiker.
  // gebruiker kiest en de keuze wordt geimplementeerd.
 
-    uitkomst = regelTransformSelected(eq, regels[binomium2Expand]);
+ console.log('naarplus gestart');
+
+    uitkomst = regelTransformSelected(eq, regels['binomium2Expand']);
     if (uitkomst.equals(eq)==false) {
       console.log("nieuwe eq:  " + uitkomst.toString());
         updateLatex(uitkomst);
